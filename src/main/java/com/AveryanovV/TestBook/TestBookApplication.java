a58763b6d3dc28ab5c475253d6b069df51eb4ef1
voxvaxnx <voxvaxnx@yandex.ru>
2023-07-25 15:59:51 +0500

    Собрал заготовку, начинаю работу

---
+++ src/main/java/com/AveryanovV/TestBook/TestBookApplication.java
@@ -0,0 +1,13 @@
+package com.AveryanovV.TestBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestBookApplication.class, args);
+	}
+
+}
